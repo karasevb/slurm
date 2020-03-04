9b48c2a57555bbf06676996ba7951dbc89a85d8c
Boris Karasev <karasev.b@gmail.com>
2020-03-04 12:14:55 +0600

    pmix: added checking for 'pipe()' return code
    Signed-off-by: Boris Karasev <karasev.b@gmail.com>

--- src/plugins/mpi/pmix/pmixp_dconn_ucx.c
+++ src/plugins/mpi/pmix/pmixp_dconn_ucx.c
@@ -850,7 +850,11 @@ static void _ucx_regio(eio_handle_t *h)
 {
 	eio_obj_t *obj;
 
-	pipe(_service_pipe);
+	if (pipe(_service_pipe) < 0) {
+		fatal("pipe: %m");
+		// TODO: propagate the error to an upper layer
+		return;
+	}
 	fd_set_nonblocking(_service_pipe[0]);
 	fd_set_nonblocking(_service_pipe[1]);
 	fd_set_close_on_exec(_service_pipe[0]);
