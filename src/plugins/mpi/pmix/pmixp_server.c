254fff80e3bdf095b3808deaefc45ccb74287cec
Boris Karasev <karasev.b@gmail.com>
2020-07-16 10:35:21 +0700

    mpi/pmix: updates copyright
    Signed-off-by: Boris Karasev <karasev.b@gmail.com>

--- src/plugins/mpi/pmix/pmixp_server.c
+++ src/plugins/mpi/pmix/pmixp_server.c
@@ -2,8 +2,9 @@
  **  pmix_server.c - PMIx server side functionality
  *****************************************************************************
  *  Copyright (C) 2014-2015 Artem Polyakov. All rights reserved.
- *  Copyright (C) 2015-2018 Mellanox Technologies. All rights reserved.
- *  Written by Artem Polyakov <artpol84@gmail.com, artemp@mellanox.com>.
+ *  Copyright (C) 2015-2020 Mellanox Technologies. All rights reserved.
+ *  Written by Artem Polyakov <artpol84@gmail.com, artemp@mellanox.com>,
+ *             Boris Karasev <karasev.b@gmail.com, boriska@mellanox.com>.
  *
  *  This file is part of Slurm, a resource management program.
  *  For details, see <https://slurm.schedmd.com/>.
